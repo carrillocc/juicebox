8809079625adcd534b934d6b9d27131e85704043
Cindy Carrillo <carrillo201994@gmail.com>
2022-11-08 21:46:52 -0600

    finished up writing some helper functions

---
+++ db/seed.js
@@ -0,0 +1,22 @@
+// inside db/seed.js
+
+// grab our client with destructuring from the export in index.js
+const {
+  client,
+  getAllUsers, // new
+} = require("./index");
+
+async function testDB() {
+  try {
+    client.connect();
+
+    const users = await getAllUsers();
+    console.log(users);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+testDB();
