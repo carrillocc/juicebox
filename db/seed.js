de1ba890f0689eaabcd41449956bf98cbcb6c0af
Cindy Carrillo <carrillo201994@gmail.com>
2022-11-11 12:39:44 -0600

    tags and post tags completed

--- db/seed.js
+++ db/seed.js
@@ -15,6 +15,8 @@ async function dropTables() {
     console.log("Starting to drop tables...");
 
     await client.query(`
+      DROP TABLE IF EXISTS post_tags;
+      DROP TABLE IF EXISTS tags;
       DROP TABLE IF EXISTS posts;
       DROP TABLE IF EXISTS users;
     `);
@@ -45,6 +47,14 @@ async function createTables() {
         title varchar(255) NOT NULL,
         content TEXT NOT NULL,
         active BOOLEAN DEFAULT true
+      );
+      CREATE TABLE tags (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE post_tags (
+        "postId" INTEGER REFERENCES posts(id) UNIQUE NOT NULL,
+        "tagId" INTEGER REFERENCES tags(id) UNIQUE NOT NULL
       );
     `);
 
