68f1ca8a9d7515ec774732e1d3598e9ed601a17f
Cindy Carrillo <carrillo201994@gmail.com>
2022-11-19 09:48:34 -0600

    completed get tag name

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -13,6 +13,16 @@ tagsRouter.get("/", async (req, res) => {
   res.send({
     tags,
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const tagName = req.params.tagName;
+  try {
+    const posts = await getPostsByTagName(tagName);
+    res.send({ posts });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
