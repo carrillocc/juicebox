c408dbae83dfe923facb4c2fd4d35b34115a3683
Cindy Carrillo <carrillo201994@gmail.com>
2022-11-20 21:17:45 -0600

    uncommented code out

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ server.use(bodyParser.json());
 const morgan = require("morgan");
 server.use(morgan("dev"));
 
-// server.use((req, res, next) => {
-//   console.log("<____Body Logger START____>");
-//   console.log(req.body);
-//   console.log("<_____Body Logger END_____>");
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
 
-//   next();
-// });
+  next();
+});
 
 const apiRouter = require("./api");
 server.use("/api", apiRouter);
